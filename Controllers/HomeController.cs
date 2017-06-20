f18fe40ea3424aa6738d2e107b52f36edec8df1f
reeshad <reeshad.khan1.cse@ulab.edu.bd>
2017-06-20 23:34:50 +0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jominder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult DeveloperHome()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Packages()
+        {
+            ViewBag.Message = "Your Packages page.";
+
+            return View();
+        }
+
+        public ActionResult PackegePlan()
+        {
+            ViewBag.Message = "Your Packages page.";
+
+            return View();
+        }
+    }
+}
